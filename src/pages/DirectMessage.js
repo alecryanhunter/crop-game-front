c4901a8ba0cf363ad758851051e7f05bae0bf1ef
Alec Hunter <alecryanhunter@gmail.com>
2023-05-31 12:26:24 -0700

    added direct message and shop

--- src/pages/DirectMessage.js
+++ src/pages/DirectMessage.js
@@ -4,7 +4,21 @@ function DirectMessage() {
     let { username } = useParams()
 
     return (
-        <h2>{username}'s Message History</h2>
+        <section className="page">
+            <h2>{username}'s Message History</h2>
+            <section className="direct subpage">
+                <section className="chat">
+                    <p className="msg sent">Hello There!</p>
+                    <p className="msg received">What's up?</p>
+                    <p className="msg received">Who is this?</p>
+                    <p className="msg sent">It's your best friend!</p>
+                </section>
+                <form>
+                    <input placeholder="Write your message"/>
+                    <button>Send</button>
+                </form>
+            </section>
+        </section>
     )
 }
 
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,7 +1,31 @@
 function Shop() {
 
     return (
-        <h2>Shop</h2>
+        <section className="page">
+            <h2>Shop</h2>
+            <section className="shop subpage">
+                <h3>Your Coins: 130</h3>
+                <h3>Expansions</h3>
+                <hr/>
+                <ul>
+                    <li>The Riverside Expansion</li>
+                </ul>
+                <h3>Titles</h3>
+                <hr/>
+                <ul>
+                    <li>Crop King</li>
+                    <li>Farmhand</li>
+                    <li>Grainee</li>
+                    <li>Potato Main</li>
+                </ul>
+                <h3>Skins</h3>
+                <hr/>
+                <ul>
+                    <li>Mega Carrot</li>
+                    <li>Pears</li>
+                </ul>
+            </section>
+        </section>
     )
 }
 
