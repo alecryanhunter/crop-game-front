6fe39ae6e9310f3f2aa892252e1731c9a9ea3f4e
Alec Hunter <alecryanhunter@gmail.com>
2023-05-31 11:53:21 -0700

    added user component. added lobby

--- src/pages/Lobby.js
+++ src/pages/Lobby.js
@@ -1,7 +1,23 @@
+import User from "../components/User"
+
 function Lobby() {
 
     return (
-        <h2>Lobby</h2>
+        <section className="page">
+            <h2>Lobby</h2>
+            <section className="subpage lobby">
+                <h3>Lobby Code: 5678</h3>
+                <User username="test_username" title="test_title"/>
+                <User username="test_username" title="test_title"/>
+                <User username="test_username" title="test_title"/>
+                <User username="test_username" title="test_title"/>
+                <section className="chat">
+                    <p>Message 1</p>
+                    <p>Message 2</p>
+                    <p>Message 3</p>
+                </section>
+            </section>
+        </section>
     )
 }
 
