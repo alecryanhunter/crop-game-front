8e77f260048054256f66764d3c78f4d2560fe292
Alec Hunter <alecryanhunter@gmail.com>
2023-06-01 13:53:43 -0700

    added login conditional render for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,25 @@
-function Home() {
+function Home({loggedIn,setLoggedIn}) {
 
     return (
         <section className="page">
             <h2>Home</h2>
             <section className="home subpage">
-                <h3><a href="/lobby">Host Game</a></h3>
-                <h3><a href="/lobby">Join Game</a></h3>
-                <h3><a href="/search/test_username">Search For A User</a></h3>
-                <h3><a href="/rules">View Rules</a></h3>
-                <hr/>
-                <input placeholder="username"/>
-                <input placeholder="password"/>
-                <button>Submit</button>
-                <p>Forgot password?</p>
-                <p>Signup</p>
+                {loggedIn ? (
+                    <>
+                    <h3><a href="/lobby">Host Game</a></h3>
+                    <h3><a href="/lobby">Join Game</a></h3>
+                    <h3><a href="/search/test_username">Search For A User</a></h3>
+                    <h3><a href="/rules">View Rules</a></h3>
+                    </>
+                ) : (
+                    <>
+                    <input placeholder="username"/>
+                    <input placeholder="password"/>
+                    <button>Submit</button>
+                    <p>Forgot password?</p>
+                    <p>Signup</p>
+                    </>
+                )}
             </section>
         </section>
     )
