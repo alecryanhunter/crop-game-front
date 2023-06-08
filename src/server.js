76b8c5c00120226ccc2e02d6bd5a9ac57f31ed70
Rhonda Stokes <rhonda@aerostokes.com>
2023-06-08 14:03:18 -0700

    heroku deploy edits

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const { Server, Origins } = require("boardgame.io/server")
+const fs = require('fs');
 const path = require("path");
 const serve = require("koa-static");
 const { CropGame } = require('./components/Game')
@@ -8,6 +9,10 @@ const { GAME_PORT, APP_PRODUCTION } = require("./config")
 const server = Server({
     games: [CropGame],
     origins: APP_PRODUCTION ? ["https://cropposition.herokuapp.com/"] : [Origins.LOCALHOST],
+    // https: {
+    //     cert: fs.readFileSync('/path/to/cert'),
+    //     key: fs.readFileSync('/path/to/key'),
+    //   },
 });
 
 
