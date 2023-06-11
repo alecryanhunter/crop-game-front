e3c5dd6c0ce7581db1866cc3a0e0f0aca7bd8ffb
Alec Hunter <alecryanhunter@gmail.com>
2023-06-11 16:26:01 -0700

    added game end screen logic

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import API from "../utils/API"
 
+import "../assets/styles/Game.css"
+
 function EndScreen({coins, matchData}) {
 
     function winnerFinder(arr) {
@@ -14,21 +16,42 @@ function EndScreen({coins, matchData}) {
             }
         }
         if (equal) {
-            return "Draw"
+            return -1
         }
 
-        return `Player ${winDex=1} Wins!`;
+        return winDex;
     }
 
     useEffect(()=>{
-        console.log(matchData);
-        winnerFinder(coins);
+        const winner = winnerFinder(coins);
+        for (let i = 0;i<matchData.length;i++) {
+            if (i === winner){
+                API.updateStats(matchData[i].name,"wins",coins[i])
+            } else {
+                API.updateStats(matchData[i].name,"losses",coins[i])
+            }
+        }
     },[])
 
     return (
         <section className="game-over">
             <h3>GAME OVER</h3>
-            <p>{winnerFinder(coins)}</p>
+            {winnerFinder(coins) === -1 ? (
+                <>
+                <p>The game ended in a draw...</p>
+                {[...Array(coins.length)].map((e,i)=> (
+                    <p key={i}><span className="player-name">{matchData[i].name}</span> earned {coins[i]} coins.</p>
+                ))}
+                </>
+            ) : (
+                <>
+                <p><span className="player-name">{matchData[winnerFinder(coins)].name}</span> won the game, earning {coins[winnerFinder(coins)]} coins.</p>
+                {[...Array(coins.length)].map((e,i)=> (
+                    (i === winnerFinder(coins) ? null : (
+                        <p key={i}><span className="player-name">{matchData[i].name}</span> lost, earning {coins[i]} coins.</p>
+                ))))}
+                </>
+            )}
         </section>
     );
 }
