1ad10748d22a4ee674280469621669a569c98670
anjaliroland <anjaligroland@gmail.com>
2023-06-11 18:22:09 -0700

    chat is kind of lobby and game

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -9,8 +9,8 @@ import { CropGameBoard } from "../components/Board";
 import { SocketIO } from "boardgame.io/multiplayer";
 import { Lobby } from "boardgame.io/react";
 import { CropLobby } from "../components/Lobby";
-import { GAME_SERVER } from "../config"
-
+import { GAME_SERVER } from "../config";
+import Chat from "../components/Chat";
 
 const CropClient = Client({ 
   game: CropGame,
@@ -19,7 +19,7 @@ const CropClient = Client({
 })
 
 
-export default function Play() {
+export default function Play({socket}) {
 
     // const [username, setUsername] = useState("");
     // const [room, setRoom] = useState("");
@@ -50,6 +50,7 @@ export default function Play() {
             lobbyServer={GAME_SERVER}
             gameComponents={[{ game: CropGame, board: CropGameBoard }]}
             />
+          <Chat socket={socket} />
           {/* {!showChat ? (
             <div className="newGameContainer">
             <input
